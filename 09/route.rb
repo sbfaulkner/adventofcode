19fddd97b986810cc16d13ed411d2f58b2abe5cf
S. Brent Faulkner <brent.faulkner@shopify.com>
2016-01-10 21:34:27 -0500

    simplification

--- 09/route.rb
+++ 09/route.rb
@@ -4,7 +4,6 @@ class Route
   end
 
   def self.load(paths)
-    @routes = nil
     @distances = Hash.new { |h, k| h[k] = {} }.tap do |distances|
       paths.each_line do |path|
         origin, destination, distance = path.match(/([a-z]+) to ([a-z]+) = (\d+)/i).captures
@@ -15,13 +14,14 @@ class Route
   end
 
   def self.all
-    @routes ||= @distances.keys.permutation.map { |cities| new(cities) }
+    @distances.keys.permutation.map { |cities| new(cities) }
   end
 
   def initialize(cities)
     @cities = cities
-    @length = @cities.each_cons(2).map { |o, d| self.class.distances.fetch(o).fetch(d) }.reduce(&:+)
   end
 
-  attr_reader :length
+  def length
+    @length ||= @cities.each_cons(2).map { |o, d| self.class.distances.fetch(o).fetch(d) }.reduce(&:+)
+  end
 end
