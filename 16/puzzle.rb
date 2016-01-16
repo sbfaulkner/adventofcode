be0c228274e2ad044c762f11b4dc1ae9dcd5e382
S. Brent Faulkner <brent.faulkner@shopify.com>
2016-01-16 18:05:07 -0500

    cleanup

--- 16/puzzle.rb
+++ 16/puzzle.rb
@@ -1,6 +1,7 @@
 # --- Day 16: Aunt Sue ---
 #
-# Your Aunt Sue has given you a wonderful gift, and you'd like to send her a thank you card. However, there's a small problem: she signed it "From, Aunt Sue".
+# Your Aunt Sue has given you a wonderful gift, and you'd like to send her a thank you card. However, there's a small
+# problem: she signed it "From, Aunt Sue".
 #
 # You have 500 Aunts named "Sue".
 #
