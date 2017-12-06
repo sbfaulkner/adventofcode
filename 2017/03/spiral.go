d94ff42139b84af31918de594823642a54f3a3a1
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-12-05 22:22:48 -0500

    02: spiral - refactor into type

--- 2017/03/spiral.go
+++ 2017/03/spiral.go
@@ -21,38 +21,56 @@ func abs(value int) int {
 	return value
 }
 
-func findMiddle(size int) (x int, y int) {
-	x = (size - 1) / 2
-	y = size / 2
+type spiral struct {
+	last int
+	size int
+}
+
+func newSpiral(last int) spiral {
+	size := int(math.Ceil(math.Sqrt(float64(last))))
+
+	return spiral{last: last, size: size}
+}
+
+func (s spiral) middle() (x, y int) {
+	x = (s.size - 1) / 2
+	y = s.size / 2
 
 	return
 }
 
-func findPosition(size, value int) (x int, y int) {
-	square := size * size
-	diff := square - value
+func (s spiral) position() (x, y int) {
+	square := s.size * s.size
+	diff := square - s.last
 
-	if size%2 == 1 {
-		x = size - 1
-		y = size - 1
+	if s.size%2 == 1 {
+		x = s.size - 1
+		y = s.size - 1
 
-		if diff >= size {
-			x = x - (size - 1)
+		if diff >= s.size {
+			x = x - (s.size - 1)
 		}
 
-		y = y - diff%size
+		y = y - diff%s.size
 	} else {
 		x = 0
 		y = 0
 
-		if diff >= size {
-			x = x + (size - 1)
+		if diff >= s.size {
+			x = x + (s.size - 1)
 		}
 
-		y = y + diff%size
+		y = y + diff%s.size
 	}
 
 	return
+}
+
+func (s spiral) distance() int {
+	x1, y1 := s.middle()
+	x2, y2 := s.position()
+
+	return abs(x2-x1) + abs(y2-y1)
 }
 
 func main() {
@@ -64,12 +82,7 @@ func main() {
 		input = i
 	}
 
-	size := int(math.Ceil(math.Sqrt(float64(input))))
+	s := newSpiral(input)
 
-	x1, y1 := findMiddle(size)
-	x2, y2 := findPosition(size, input)
-
-	distance := abs(x2-x1) + abs(y2-y1)
-
-	fmt.Println(distance)
+	fmt.Println(s.distance())
 }
