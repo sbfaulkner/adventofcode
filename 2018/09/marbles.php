d909eb3b929514986f3c6bfddb722cafed9bf478
S. Brent Faulkner <brent.faulkner@shopify.com>
2018-12-16 17:54:03 -0500

    day 9 - part 2

--- 2018/09/marbles.php
+++ 2018/09/marbles.php
@@ -4,12 +4,12 @@
 const INPUT = __DIR__ . "/input";
 
 class Marbles {
-  private $marbles = array(0);
-  private $current = 0;
-  private $marble = 0;
+  private $marbles;
+  private $current;
+  private $marble;
 
-  public function Marbles() {
-    $this->marbles = array(0);
+  public function __construct() {
+    $this->marbles = new Ds\Deque([0]);
     $this->current = 0;
     $this->marble = 0;
   }
@@ -17,34 +17,17 @@ class Marbles {
   public function play() {
     $score = 0;
     $this->marble++;
-    $count = count($this->marbles);
 
     if ($this->marble % 23 == 0) {
       $score += $this->marble;
-      $this->current -= 7;
-      $this->current = ($count + ($this->current % $count)) % $count;
-      $score += $this->marbles[$this->current];
-      array_splice($this->marbles, $this->current, 1);
+      $this->marbles->rotate(-7);
+      $score += $this->marbles->shift();
     } else {
-      $this->current = ($this->current + 1) % $count + 1;
-      array_splice($this->marbles, $this->current, 0, array($this->marble));
+      $this->marbles->rotate(2);
+      $this->marbles->unshift($this->marble);
     }
 
     return $score;
-  }
-
-  public function print() {
-    $count = count($this->marbles);
-
-    for ($m = 0; $m < $count; $m++) {
-      if ($m == $this->current) {
-        printf(" (%d)", $this->marbles[$m]);
-      } else {
-        printf(" %d", $this->marbles[$m]);
-      }
-    }
-
-    print("\n");
   }
 }
 
@@ -54,27 +37,17 @@ while ($line = fgets($finput)) {
   preg_match('/(?<players>[0-9]+) players; last marble is worth (?<limit>[0-9]+) points/', $line, $matches);
 
   $players = $matches["players"];
-  $limit = $matches["limit"];
+  $limit = $matches["limit"] * 100;
 
   $scores = array_fill(0, $players, 0);
 
   $marbles = new Marbles();
-
-  if ($debug) {
-    print("[-] ");
-    $marbles->print();
-  }
 
   $player = 0;
 
   for ($turn = 0; $turn < $limit; $turn++) {
     $score = $marbles->play();
     $scores[$player] += $score;
-
-    if ($debug) {
-      printf("[%d]", $player+1);
-      $marbles->print();
-    }
 
     $player++;
     $player %= $players;
