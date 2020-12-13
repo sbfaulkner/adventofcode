69db2047a01e14c3de57d74192ced5823c572dd3
S. Brent Faulkner <brent.faulkner@shopify.com>
2020-12-13 15:54:42 -0500

    remove 2016 from master

--- 2016/10/player.rb
+++
@@ -1,9 +0,0 @@
-class Player
-  def initialize(digits)
-    @digits = digits.scan(/(\d)(\1*)/).map(&:join)
-  end
-
-  def say
-    @digits.map { |segment| "#{segment.length}#{segment[0]}" }.join
-  end
-end
