d74c1a1dffd1e7e197a4ce5557e621876ad80748
S. Brent Faulkner <sbfaulkner@gmail.com>
2021-02-12 21:04:15 -0500

    working?

--- pkg/advent/math.go
+++ pkg/advent/math.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"strconv"
 	"strings"
 	"unicode"
@@ -21,6 +22,17 @@ type token struct {
 	t int
 	i int
 	s string
+}
+
+func (t token) String() string {
+	switch t.t {
+	case eofToken:
+		return "<EOF>"
+	case intToken:
+		return fmt.Sprintf("<%d>", t.i)
+	default:
+		return fmt.Sprintf("<%s>", t.s)
+	}
 }
 
 // MathProblem is an expression to be solved
@@ -49,6 +61,28 @@ func NewMathProblem(problem string) *MathProblem {
 	return &p
 }
 
+// Evaluate solves a math problem
+func (p *MathProblem) Evaluate(fp OperatorPrecedence) (int, error) {
+	e := mathEvaluator{
+		rd:          strings.NewReader(string(*p)),
+		fprecedence: fp,
+	}
+
+	fstate := evaluateLeftValue
+
+	for e.getToken() {
+		fs, err := fstate(&e)
+		if err != nil {
+			return 0, err
+		}
+		fstate = fs
+	}
+
+	i := e.popToken().i
+
+	return i, nil
+}
+
 // OperatorPrecedence to use when evaluating an expression
 type OperatorPrecedence func(o1, o2 string) bool
 
@@ -66,34 +100,28 @@ func AdvancedMathPrecedence(o1, o2 string) bool {
 	return o1 == "+"
 }
 
-// Evaluate solves a math problem
-func (p *MathProblem) Evaluate(f OperatorPrecedence) (int, error) {
-	e := mathEvaluator{
-		rd: strings.NewReader(string(*p)),
-		f:  f,
-	}
-
-	if err := evaluateProblem(&e); err != nil {
-		return 0, err
-	}
-	return e.popInt(), nil
-}
-
 type mathEvaluator struct {
-	rd    *strings.Reader
-	err   error
-	depth int
-	tok   token
-	st    []token
-	f     OperatorPrecedence
+	rd          *strings.Reader
+	err         error
+	depth       int
+	tok         token
+	unget       bool
+	st          []token
+	fstate      mathEvaluatorState
+	fprecedence OperatorPrecedence
 }
 
 func (e *mathEvaluator) getToken() bool {
+	if e.unget {
+		e.unget = false
+		return true
+	}
+
 	for {
 		ch, _, err := e.rd.ReadRune()
 		if err == io.EOF {
 			e.tok.t = eofToken
-			return true
+			return len(e.st) > 1
 		}
 
 		if err != nil {
@@ -128,6 +156,16 @@ func (e *mathEvaluator) getToken() bool {
 			e.err = fmt.Errorf("syntax error at %c", ch)
 			return false
 		}
+	}
+}
+
+func (e *mathEvaluator) ungetToken() {
+	if e.unget {
+		log.Fatal("unable to unget multiple tokens")
+	}
+
+	if e.tok.t != eofToken {
+		e.unget = true
 	}
 }
 
@@ -166,116 +204,114 @@ func (e *mathEvaluator) getNumber() bool {
 	return true
 }
 
-func (e *mathEvaluator) pushToken() {
-	e.st = append(e.st, e.tok)
+func (e *mathEvaluator) pushToken(t token) {
+	e.st = append(e.st, t)
 }
 
 func (e *mathEvaluator) pushInt(i int) {
-	e.tok.t = intToken
-	e.tok.i = i
-	e.pushToken()
+	e.pushToken(token{t: intToken, i: i})
 }
 
 func (e *mathEvaluator) popToken() token {
+	if len(e.st) == 0 {
+		return token{t: eofToken}
+	}
+
 	t := e.st[len(e.st)-1]
 	e.st = e.st[0 : len(e.st)-1]
 	return t
 }
 
-func (e *mathEvaluator) popInt() int {
-	return e.popToken().i
-}
+type mathEvaluatorState func(*mathEvaluator) (mathEvaluatorState, error)
 
-func (e *mathEvaluator) popString() string {
-	return e.popToken().s
-}
+func evaluateLeftValue(e *mathEvaluator) (mathEvaluatorState, error) {
+	switch e.tok.t {
+	case intToken:
+		e.pushToken(e.tok)
+		return evaluateOperator, nil
 
-func evaluateProblem(e *mathEvaluator) error {
-	e.depth++
-	defer func() { e.depth-- }()
-
-	for e.getToken() {
-		switch e.tok.t {
-		case intToken:
-			e.pushToken()
-			return evaluateLeft(e)
-
-		case lparenToken:
-			err := evaluateProblem(e)
-			if err != nil {
-				return err
-			}
-			return evaluateLeft(e)
-
-		default:
-			return fmt.Errorf("syntax error at %s (expected integer)", e.tok.s)
-		}
+	case lparenToken:
+		e.pushToken(e.tok)
+		return evaluateLeftValue, nil
 	}
 
-	return e.err
+	return nil, fmt.Errorf("syntax error at %s (expected integer)", e.tok.s)
 }
 
-func evaluateLeft(e *mathEvaluator) error {
-	if !e.getToken() {
-		return e.err
-	}
+func evaluateOperator(e *mathEvaluator) (mathEvaluatorState, error) {
+	if e.tok.t == rparenToken {
+		i := e.popToken()
+		e.popToken()
 
-	if e.depth > 1 {
-		if e.tok.t == rparenToken {
-			return nil
-		}
-	} else {
-		if e.tok.t == eofToken {
-			return nil
-		}
+		e.pushToken(i)
+
+		return evaluateOperator, nil
 	}
 
 	if e.tok.t != opToken {
-		return fmt.Errorf("syntax error at %s (expected operator)", e.tok.s)
+		return nil, fmt.Errorf("syntax error at %s (expected operator)", e.tok.s)
 	}
 
-	e.pushToken()
-	if err := evaluateOp(e); err != nil {
-		return err
-	}
+	e.pushToken(e.tok)
 
-	return evaluateLeft(e)
+	return evaluateRightValue, nil
 }
 
-func evaluateOp(e *mathEvaluator) error {
-	if !e.getToken() {
-		return e.err
-	}
-
+func evaluateRightValue(e *mathEvaluator) (mathEvaluatorState, error) {
 	switch e.tok.t {
 	case intToken:
-		e.pushToken()
-		return evaluateExpression(e)
+		e.pushToken(e.tok)
+		return evaluateExpression, nil
 
 	case lparenToken:
-		err := evaluateProblem(e)
-		if err != nil {
-			return err
-		}
-		return evaluateExpression(e)
+		e.pushToken(e.tok)
+		return evaluateLeftValue, nil
 	}
 
-	return fmt.Errorf("syntax error at %s (expected rval)", e.tok.s)
+	return nil, fmt.Errorf("syntax error at %s (expected rval)", e.tok.s)
 }
 
-func evaluateExpression(e *mathEvaluator) error {
-	l := e.popInt()
-	op := e.popString()
-	r := e.popInt()
+func evaluateExpression(e *mathEvaluator) (mathEvaluatorState, error) {
+	r := e.popToken()
+	op := e.popToken()
 
-	switch op {
-	case "+":
-		e.pushInt(l + r)
-	case "*":
-		e.pushInt(l * r)
-	default:
-		return fmt.Errorf("unknown operator at %s", op)
+	if e.tok.t == rparenToken && op.t == lparenToken {
+		e.pushToken(r)
+		return evaluateExpression, nil
 	}
 
-	return nil
+	if op.t != opToken {
+		if op.t != eofToken {
+			e.pushToken(op)
+		}
+		e.pushToken(r)
+		e.ungetToken()
+
+		return evaluateOperator, nil
+	}
+
+	if e.tok.t == opToken && !e.fprecedence(op.s, e.tok.s) {
+		e.pushToken(op)
+		e.pushToken(r)
+		e.pushToken(e.tok)
+
+		return evaluateRightValue, nil
+	}
+
+	if e.tok.t != eofToken {
+		e.ungetToken()
+	}
+
+	l := e.popToken()
+
+	switch op.s {
+	case "+":
+		e.pushInt(l.i + r.i)
+	case "*":
+		e.pushInt(l.i * r.i)
+	default:
+		return nil, fmt.Errorf("unknown operator at %s", op)
+	}
+
+	return evaluateExpression, nil
 }
