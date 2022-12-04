9455a9e52ac7d9372181ea698ded9c456f1429e8
S. Brent Faulkner <brent.faulkner@shopify.com>
2022-12-03 21:34:00 -0500

    default day based on input files

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
 
 pub struct Config {
     pub day: u8,
     pub input: BufReader<File>,
+}
+
+fn current_day() -> u8 {
+    (1..=25)
+        .take_while(|d| Path::new(&format!("input/day{:02}.txt", d)).is_file())
+        .last()
+        .unwrap_or(1)
 }
 
 impl Config {
@@ -12,10 +20,22 @@ impl Config {
         // skip the first argument, which is the program name
         args.next();
 
+        let current_day = current_day();
+
         let day: u8 = match args.next() {
-            Some(arg) => arg.parse().map_err(|_| "Day must be a number")?,
-            None => 3,
+            Some(arg) => arg
+                .parse()
+                .map_err(|_| "Day must be a number between 1 and 25")?,
+            None => current_day,
         };
+
+        if day < 1 || day > 25 {
+            return Err("Day must be between 1 and 25".into());
+        }
+
+        if day > current_day {
+            return Err(format!("Day {} is not yet implemented", day).into());
+        }
 
         let f = File::open(format!("input/day{:02}.txt", day))
             .map_err(|err| format!("Problem opening the file: {}", err))?;
