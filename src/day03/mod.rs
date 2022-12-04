2e3e86a01f114b3a9694bca2aebd15f9b99d8459
S. Brent Faulkner <brent.faulkner@shopify.com>
2022-12-03 20:43:26 -0500

    day 3 - part 2 - solved

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,8 +1,10 @@
-use std::{collections::HashSet, io::BufRead};
+use std::collections::HashSet;
+use std::io::BufRead;
 
 pub fn run(input: impl BufRead) {
     let rucksacks: Vec<Rucksack> = read_rucksacks(input);
     println!("* Part 1: {}", total_priority(&rucksacks));
+    println!("* Part 2: {}", total_badges(&rucksacks));
 }
 
 fn read_rucksacks(input: impl BufRead) -> Vec<Rucksack> {
@@ -14,10 +16,34 @@ fn read_rucksacks(input: impl BufRead) -> Vec<Rucksack> {
 }
 
 fn total_priority(rucksacks: &Vec<Rucksack>) -> Priority {
+    rucksacks.iter().map(|r| r.priority()).sum()
+}
+
+fn total_badges(rucksacks: &Vec<Rucksack>) -> Priority {
     rucksacks
-        .iter()
-        .map(|r| r.priority())
+        .chunks(3)
+        .map(|r| ElfGroup::new(r))
+        .map(|g| g.badge)
         .sum()
+}
+
+struct ElfGroup {
+    badge: Priority,
+}
+
+impl ElfGroup {
+    fn new(rucksacks: &[Rucksack]) -> Self {
+        let badge = rucksacks
+            .iter()
+            .map(|r| r.contents())
+            .reduce(|a, b| &a & &b)
+            .expect("expected intersection")
+            .iter()
+            .next()
+            .expect("expected thing")
+            .priority();
+        ElfGroup { badge }
+    }
 }
 
 struct Rucksack {
@@ -33,6 +59,14 @@ impl Rucksack {
             .expect("no intersection")
             .priority()
     }
+
+    fn contents(&self) -> HashSet<Item> {
+        self.compartments[0]
+            .contents
+            .union(&self.compartments[1].contents)
+            .map(|&t| t)
+            .collect()
+    }
 }
 
 impl From<&str> for Rucksack {
@@ -46,20 +80,20 @@ impl From<&str> for Rucksack {
 }
 
 struct Compartment {
-    contents: HashSet<Thing>,
+    contents: HashSet<Item>,
 }
 
 impl From<&str> for Compartment {
     fn from(s: &str) -> Self {
-        let contents = s.chars().map(|c| Thing::from(c)).collect();
+        let contents = s.chars().map(|c| Item::from(c)).collect();
         Compartment { contents }
     }
 }
 
-#[derive(Eq, Hash, PartialEq)]
-struct Thing(char);
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+struct Item(char);
 
-impl Thing {
+impl Item {
     fn priority(&self) -> Priority {
         if self.0.is_ascii_lowercase() {
             self.0 as Priority - 'a' as Priority + 1
@@ -69,10 +103,10 @@ impl Thing {
     }
 }
 
-impl From<char> for Thing {
+impl From<char> for Item {
     fn from(c: char) -> Self {
         assert!(c.is_ascii_alphabetic());
-        Thing(c)
+        Item(c)
     }
 }
 
@@ -103,6 +137,19 @@ CrZsJsPPZsGzwwsLwLmpwMDw
     }
 
     #[test]
+    fn test_total_badges() {
+        let rucksacks = read_rucksacks(INPUT);
+        assert_eq!(total_badges(&rucksacks), 70);
+    }
+
+    #[test]
+    fn test_elf_group_badge() {
+        let rucksacks = read_rucksacks(INPUT);
+        assert_eq!(ElfGroup::new(&rucksacks[0..3]).badge, 18);
+        assert_eq!(ElfGroup::new(&rucksacks[3..6]).badge, 52);
+    }
+
+    #[test]
     fn test_rucksack_priority() {
         let rucksacks = read_rucksacks(INPUT);
         assert_eq!(rucksacks[0].priority(), 16);
@@ -115,11 +162,11 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
     #[test]
     fn test_thing_priority() {
-        assert_eq!(Thing::from('a').priority(), 1);
-        assert_eq!(Thing::from('b').priority(), 2);
-        assert_eq!(Thing::from('z').priority(), 26);
-        assert_eq!(Thing::from('A').priority(), 27);
-        assert_eq!(Thing::from('B').priority(), 28);
-        assert_eq!(Thing::from('Z').priority(), 52);
+        assert_eq!(Item::from('a').priority(), 1);
+        assert_eq!(Item::from('b').priority(), 2);
+        assert_eq!(Item::from('z').priority(), 26);
+        assert_eq!(Item::from('A').priority(), 27);
+        assert_eq!(Item::from('B').priority(), 28);
+        assert_eq!(Item::from('Z').priority(), 52);
     }
 }
