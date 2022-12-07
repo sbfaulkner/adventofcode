6adcdf293afc12c7c3dfd8d8863ea09bd51418e9
S. Brent Faulkner <brent.faulkner@shopify.com>
2022-12-06 21:33:47 -0500

    rm 2018

--- 2018/01/calibrate1.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-INPUT = File.expand_path('input', __dir__)
-
-frequency = File.readlines(INPUT).map(&:to_i).reduce(0, &:+)
-
-STDOUT.puts "Frequency: #{frequency}"
