86e1073f8dbf4572f4c26c53e740f51930e361f7
S. Brent Faulkner <brent.faulkner@shopify.com>
2022-12-16 19:26:46 -0500

    omit edges to start or from end

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,5 +1,5 @@
 use crate::measure;
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::fmt::Display;
 use std::fmt::Formatter;
 use std::io::BufRead;
@@ -17,55 +17,57 @@ pub fn run(input: impl BufRead) {
     });
 }
 
-#[derive(Debug, Eq, Hash, PartialEq, PartialOrd)]
+#[derive(Debug, Eq, Hash, PartialEq)]
 struct Position(usize, usize);
 
-impl Ord for Position {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.0.cmp(&other.0).then(self.1.cmp(&other.1))
-    }
+struct Node {
+    height: u32,
+    start: bool,
+    end: bool,
 }
 
-struct Cell(char);
-
-impl Cell {
-    fn is_start(&self) -> bool {
-        self.0 == 'S'
-    }
-
-    fn is_end(&self) -> bool {
-        self.0 == 'E'
+impl Node {
+    fn new(c: char) -> Self {
+        match c {
+            'S' => Self { height: 0, start: true, end: false },
+            'E' => Self { height: 25, start: false, end: true },
+            _ => Self { height: c as u32 - 'a' as u32, start: false, end: false }
+        }
     }
 
     fn can_access(&self, other: &Self) -> bool {
-        other.height() <= self.height() + 1
+        !other.start && other.height <= self.height + 1
     }
+}
 
-    fn height(&self) -> u32 {
-        match self.0 {
-            'S' => 'a' as u32,
-            'E' => 'z' as u32,
-            _ => self.0 as u32,
+impl Display for Node {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.start {
+            write!(f, "S")
+        } else if self.end {
+            write!(f, "E")
+        } else {
+            write!(f, "{}", (self.height + 'a' as u32) as u8 as char)
         }
     }
 }
 
 struct Graph<'m> {
     map: &'m Map,
-    graph: BTreeMap<Position, Vec<Position>>,
+    graph: HashMap<Position, Vec<Position>>,
     start: Position,
     end: Position,
 }
 
 struct Map {
-    data: Vec<Vec<Cell>>,
+    data: Vec<Vec<Node>>,
 }
 
 impl Map {
     fn read(input: impl BufRead) -> Self {
-        let data: Vec<Vec<Cell>> = input
+        let data: Vec<Vec<Node>> = input
             .lines()
-            .map(|l| l.expect("expected line").chars().map(|c| Cell(c)).collect())
+            .map(|l| l.expect("expected line").chars().map(|c| Node::new(c)).collect())
             .collect();
 
         Self { data }
@@ -75,32 +77,36 @@ impl Map {
         let mut start = Position(0, 0);
         let mut end = Position(0, 0);
 
-        let mut graph = BTreeMap::new();
+        let mut graph = HashMap::new();
 
         for (r, row) in self.data.iter().enumerate() {
-            for (c, cell) in row.iter().enumerate() {
-                if cell.is_start() {
+            for (c, n) in row.iter().enumerate() {
+                if n.start {
                     start = Position(r, c);
-                } else if cell.is_end() {
+                } else if n.end {
                     end = Position(r, c);
                 }
 
-                let entry = graph.entry(Position(r, c)).or_insert(Vec::with_capacity(4));
+                let edges = graph.entry(Position(r, c)).or_insert(Vec::with_capacity(4));
 
-                if r > 0 && cell.can_access(&self.data[r - 1][c]) {
-                    entry.push(Position(r - 1, c));
+                if n.end {
+                    continue;
                 }
 
-                if c > 0 {
-                    entry.push(Position(r, c - 1));
+                if r > 0 && n.can_access(&self.data[r - 1][c]) {
+                    edges.push(Position(r - 1, c));
                 }
 
-                if c < row.len() - 1 {
-                    entry.push(Position(r, c + 1));
+                if c > 0 && n.can_access(&self.data[r][c - 1]) {
+                    edges.push(Position(r, c - 1));
                 }
 
-                if r < self.data.len() - 1 {
-                    entry.push(Position(r + 1, c));
+                if c < row.len() - 1 && n.can_access(&self.data[r][c + 1]) {
+                    edges.push(Position(r, c + 1));
+                }
+
+                if r < self.data.len() - 1 && n.can_access(&self.data[r + 1][c]) {
+                    edges.push(Position(r + 1, c));
                 }
             }
         }
@@ -117,8 +123,8 @@ impl Map {
 impl Display for Map {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         for row in self.data.iter() {
-            for cell in row.iter() {
-                write!(f, "{}", cell.0)?;
+            for n in row.iter() {
+                write!(f, "{}", n)?;
             }
             writeln!(f)?;
         }
@@ -155,10 +161,18 @@ abdefghi
         assert_eq!(graph.end, Position(2, 5));
 
         // spotcheck several positions
+        // start corner
         assert_eq!(graph.graph[&Position(0, 0)], vec![Position(0, 1), Position(1, 0)]);
+        // other corners
         assert_eq!(graph.graph[&Position(0, 7)], vec![Position(0, 6), Position(1, 7)]);
         assert_eq!(graph.graph[&Position(4, 0)], vec![Position(3, 0), Position(4, 1)]);
         assert_eq!(graph.graph[&Position(4, 7)], vec![Position(3, 7), Position(4, 6)]);
+
+        // shouldn't go to start
+        assert_eq!(graph.graph[&Position(0, 1)], vec![Position(0, 2), Position(1, 1)]);
+        // going to end
         assert_eq!(graph.graph[&Position(2, 4)], vec![Position(1, 4), Position(2, 3), Position(2, 5), Position(3, 4)]);
+        // end goes nowhere
+        assert_eq!(graph.graph[&Position(2, 5)], vec![]);
     }
 }
