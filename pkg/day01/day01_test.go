910179424b61da02592c282a6a55d19f5b84b9f6
S. Brent Faulkner <brent.faulkner@shopify.com>
2024-12-01 16:40:59 -0500

    day 1 - part 2

--- pkg/day01/day01_test.go
+++ pkg/day01/day01_test.go
@@ -13,38 +13,39 @@ import (
 
 func TestDay01(t *testing.T) {
 	tests := []struct {
-		inputPath string
-		want      int
+		inputPath           string
+		wantDistance        int
+		wantSimilarityScore int
 	}{
-		{inputPath: "testdata/example1.txt", want: 11},
-		{inputPath: "testdata/input.txt", want: 1651298},
+		{inputPath: "testdata/example1.txt", wantDistance: 11, wantSimilarityScore: 31},
+		{inputPath: "testdata/input.txt", wantDistance: 1651298, wantSimilarityScore: 21306195},
 	}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("Part 1: %s", test.inputPath), func(t *testing.T) {
-			input, err := os.ReadFile(test.inputPath)
-			if err != nil {
-				t.Fatal(err)
-			}
+		input, err := os.ReadFile(test.inputPath)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-			lists := make([][]int, 2)
+		lists := make([][]int, 2)
 
-			scanner := bufio.NewScanner(bytes.NewReader(input))
-			for scanner.Scan() {
-				line := scanner.Text()
-				fields := strings.Fields(line)
+		scanner := bufio.NewScanner(bytes.NewReader(input))
+		for scanner.Scan() {
+			line := scanner.Text()
+			fields := strings.Fields(line)
 
-				for f, field := range fields {
-					id, err := strconv.Atoi(field)
-					if err != nil {
-						t.Fatal(err)
-					}
-
-					i, _ := slices.BinarySearch(lists[f], id)
-					lists[f] = slices.Insert(lists[f], i, id)
+			for f, field := range fields {
+				id, err := strconv.Atoi(field)
+				if err != nil {
+					t.Fatal(err)
 				}
-			}
 
+				i, _ := slices.BinarySearch(lists[f], id)
+				lists[f] = slices.Insert(lists[f], i, id)
+			}
+		}
+
+		t.Run(fmt.Sprintf("Part 1: %s", test.inputPath), func(t *testing.T) {
 			totalDistance := 0
 
 			for i, id := range lists[0] {
@@ -56,8 +57,26 @@ func TestDay01(t *testing.T) {
 				}
 			}
 
-			if totalDistance != test.want {
-				t.Errorf("totalDistance: got %d, want %d", totalDistance, test.want)
+			if totalDistance != test.wantDistance {
+				t.Errorf("totalDistance: got %d, want %d", totalDistance, test.wantDistance)
+			}
+		})
+
+		t.Run(fmt.Sprintf("Part 2: %s", test.inputPath), func(t *testing.T) {
+			locationCounts := make(map[int]int)
+
+			for _, id := range lists[1] {
+				locationCounts[id]++
+			}
+
+			similarityScore := 0
+
+			for _, id := range lists[0] {
+				similarityScore += id * locationCounts[id]
+			}
+
+			if similarityScore != test.wantSimilarityScore {
+				t.Errorf("similarityScore: got %d, want %d", similarityScore, test.wantSimilarityScore)
 			}
 		})
 	}
