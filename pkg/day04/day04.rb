c17ebc3fae2d3a77a236feef1cb2fe62869083c3
S. Brent Faulkner <brent.faulkner@shopify.com>
2024-12-04 22:27:28 -0500

    day 4 - part 1

--- pkg/day04/day04.rb
+++ pkg/day04/day04.rb
@@ -1,15 +1,46 @@
 class Day04
+  DIRECTIONS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+  ]
+
   def initialize(path)
     fullpath = File.expand_path(path, __dir__)
 
-    File.readlines(fullpath)
+    @rows = File.readlines(fullpath).map(&:chomp)
   end
 
   def part1
-    0
+    @rows.map.with_index do |row, r|
+      row.chars.map.with_index do |ch, c|
+        (ch == "X" ? DIRECTIONS.count { |dr, dc| match(r+dr, c+dc, "MAS", dr, dc) } : 0)
+      end.sum
+    end.sum
   end
 
   def part2
     0
+  end
+
+  private
+
+  def match(r, c, word, dr, dc)
+    # matched the word
+    return true if word.empty?
+
+    # bounds checking
+    return false if r < 0 || c < 0
+    return false if r >= @rows.length || c >= @rows[r].length
+
+    # not a match
+    return false if @rows[r][c] != word[0]
+
+    match(r+dr, c+dc, word[1..], dr, dc)
   end
 end
--- pkg/day04/day04_test.rb
+++ pkg/day04/day04_test.rb
@@ -9,13 +9,13 @@ require_relative "day04"
 class Day04Test < Minitest::Test
   def test_part1_example1
     input = Day04.new("testdata/example1.txt")
-    assert_equal(161, input.part1)
+    assert_equal(18, input.part1)
   end
 
-  # def test_part1
-  #   input = Day04.new("testdata/input.txt")
-  #   assert_equal(189600467, input.part1)
-  # end
+  def test_part1
+    input = Day04.new("testdata/input.txt")
+    assert_equal(189600467, input.part1)
+  end
 
   # def test_part2_example2
   #   input = Day04.new("testdata/example2.txt")
