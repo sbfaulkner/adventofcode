f7f179d345fa24232c51e1ffb31ef8341fe6f059
S. Brent Faulkner <brent.faulkner@shopify.com>
2024-12-05 18:50:25 -0500

    go solve day 4

--- pkg/day04/day04.go
+++ pkg/day04/day04.go
@@ -1,11 +1,15 @@
 package day04
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 )
 
-type input struct{}
+type input struct {
+	rows []string
+}
 
 func load(inputPath string) (*input, error) {
 	file, err := os.Open(inputPath)
@@ -14,17 +18,112 @@ func load(inputPath string) (*input, error) {
 	}
 	defer file.Close()
 
-	// scanner := bufio.NewScanner(file)
+	input := input{
+		rows: []string{},
+	}
 
-	input := input{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		input.rows = append(input.rows, scanner.Text())
+	}
 
 	return &input, nil
 }
 
 func (i *input) part1() int {
-	return 0
+	count := 0
+
+	for r, row := range i.rows {
+		for c := range row {
+			count += i.search(r, c, "XMAS")
+		}
+	}
+
+	return count
+}
+
+type direction struct {
+	dr int
+	dc int
+}
+
+var DIRECTIONS = []direction{
+	{0, 1},
+	{1, 1},
+	{1, 0},
+	{1, -1},
+	{0, -1},
+	{-1, -1},
+	{-1, 0},
+	{-1, 1},
+}
+
+func (i *input) search(r, c int, target string) int {
+	if i.rows[r][c] != target[0] {
+		return 0
+	}
+
+	count := 0
+
+	for _, dir := range DIRECTIONS {
+		if i.matchDirection(r+dir.dr, c+dir.dc, target[1:], dir) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (i *input) matchDirection(r, c int, target string, dir direction) bool {
+	if r < 0 || r >= len(i.rows) || c < 0 || c >= len(i.rows[r]) {
+		return false
+	}
+
+	if i.rows[r][c] != target[0] {
+		return false
+	}
+
+	if len(target) == 1 {
+		return true
+	}
+
+	return i.matchDirection(r+dir.dr, c+dir.dc, target[1:], dir)
 }
 
 func (i *input) part2() int {
-	return 0
+	count := 0
+
+	for r, row := range i.rows[1 : len(i.rows)-1] {
+		for c := range row[1 : len(row)-1] {
+			if i.xMasAt(r+1, c+1) {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+var SEQUENCES = [][]byte{
+	{'M', 'M', 'S', 'S'},
+	{'S', 'M', 'M', 'S'},
+	{'S', 'S', 'M', 'M'},
+	{'M', 'S', 'S', 'M'},
+}
+
+func (i *input) xMasAt(r, c int) bool {
+	if i.rows[r][c] != 'A' {
+		return false
+	}
+
+	sequence := []byte{i.rows[r-1][c-1], i.rows[r-1][c+1], i.rows[r+1][c+1], i.rows[r+1][c-1]}
+
+	for _, seq := range SEQUENCES {
+		if bytes.Equal(sequence, seq) {
+			return true
+		}
+	}
+
+	return false
 }
--- pkg/day04/day04_test.go
+++ pkg/day04/day04_test.go
@@ -10,7 +10,7 @@ func TestDay04Part1Example1(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	want := 161
+	want := 18
 	got := input.part1()
 
 	if got != want {
@@ -18,44 +18,44 @@ func TestDay04Part1Example1(t *testing.T) {
 	}
 }
 
-// func TestDay04Part1(t *testing.T) {
-// 	input, err := load("testdata/input.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestDay04Part1(t *testing.T) {
+	input, err := load("testdata/input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	want := 189600467
-// 	got := input.part1()
+	want := 2646
+	got := input.part1()
 
-// 	if got != want {
-// 		t.Errorf("got %d, want %d", got, want)
-// 	}
-// }
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
 
-// func TestDay04Part2Example2(t *testing.T) {
-// 	input, err := load("testdata/example2.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestDay04Part2Example1(t *testing.T) {
+	input, err := load("testdata/example1.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	want := 48
-// 	got := input.part2()
+	want := 9
+	got := input.part2()
 
-// 	if got != want {
-// 		t.Errorf("got %d, want %d", got, want)
-// 	}
-// }
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
 
-// func TestDay04Part2(t *testing.T) {
-// 	input, err := load("testdata/input.txt")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestDay04Part2(t *testing.T) {
+	input, err := load("testdata/input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	want := 107069718
-// 	got := input.part2()
+	want := 2000
+	got := input.part2()
 
-// 	if got != want {
-// 		t.Errorf("got %d, want %d", got, want)
-// 	}
-// }
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
