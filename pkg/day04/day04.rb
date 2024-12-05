6269bd6f027dfbe66fb39417faf44280905ce6b8
S. Brent Faulkner <brent.faulkner@shopify.com>
2024-12-04 23:17:14 -0500

    day 4 - part 2

--- pkg/day04/day04.rb
+++ pkg/day04/day04.rb
@@ -1,5 +1,29 @@
 class Day04
-  DIRECTIONS = [
+  def initialize(path)
+    fullpath = File.expand_path(path, __dir__)
+
+    @rows = File.readlines(fullpath).map(&:chomp)
+  end
+
+  def part1
+    @rows.map.with_index do |row, r|
+      row.chars.map.with_index do |_, c|
+        count_xmas(r, c)
+      end.sum
+    end.sum
+  end
+
+  def part2
+    @rows[1..-2].map.with_index do |row, r|
+      row.chars[1..-2].map.with_index do |_,c|
+        x_mas?(r + 1, c + 1) ? 1 : 0
+      end.sum
+    end.sum
+  end
+
+  private
+
+  XMAS_DIRECTIONS = [
     [-1, -1],
     [-1, 0],
     [-1, 1],
@@ -10,25 +34,10 @@ class Day04
     [0, -1],
   ]
 
-  def initialize(path)
-    fullpath = File.expand_path(path, __dir__)
-
-    @rows = File.readlines(fullpath).map(&:chomp)
+  def count_xmas(r, c)
+    return 0 unless @rows[r][c] == "X"
+    XMAS_DIRECTIONS.count { |dr, dc| match(r+dr, c+dc, "MAS", dr, dc) }
   end
-
-  def part1
-    @rows.map.with_index do |row, r|
-      row.chars.map.with_index do |ch, c|
-        (ch == "X" ? DIRECTIONS.count { |dr, dc| match(r+dr, c+dc, "MAS", dr, dc) } : 0)
-      end.sum
-    end.sum
-  end
-
-  def part2
-    0
-  end
-
-  private
 
   def match(r, c, word, dr, dc)
     # matched the word
@@ -42,5 +51,30 @@ class Day04
     return false if @rows[r][c] != word[0]
 
     match(r+dr, c+dc, word[1..], dr, dc)
+  end
+
+  X_MAS_DIRECTIONS = [
+    [-1, -1],
+    [-1, 1],
+    [1, 1],
+    [1, -1],
+  ]
+
+  def x_mas?(r, c)
+    return false unless @rows[r][c] == "A"
+
+    X_MAS_DIRECTIONS.each_with_index do |(dr, dc), i|
+      next if @rows[r+dr][c+dc] != "M"
+      dr, dc = X_MAS_DIRECTIONS[(i+1)%4]
+      next if @rows[r+dr][c+dc] != "M"
+      dr, dc = X_MAS_DIRECTIONS[(i+2)%4]
+      next if @rows[r+dr][c+dc] != "S"
+      dr, dc = X_MAS_DIRECTIONS[(i+3)%4]
+      next if @rows[r+dr][c+dc] != "S"
+
+      return true
+    end
+
+    false
   end
 end
--- pkg/day04/day04_test.rb
+++ pkg/day04/day04_test.rb
@@ -14,16 +14,16 @@ class Day04Test < Minitest::Test
 
   def test_part1
     input = Day04.new("testdata/input.txt")
-    assert_equal(189600467, input.part1)
+    assert_equal(2646, input.part1)
   end
 
-  # def test_part2_example2
-  #   input = Day04.new("testdata/example2.txt")
-  #   assert_equal(48, input.part2)
-  # end
+  def test_part2_example1
+    input = Day04.new("testdata/example1.txt")
+    assert_equal(9, input.part2)
+  end
 
-  # def test_part2
-  #   input = Day04.new("testdata/input.txt")
-  #   assert_equal(107069718, input.part2)
-  # end
+  def test_part2
+    input = Day04.new("testdata/input.txt")
+    assert_equal(2000, input.part2)
+  end
 end
