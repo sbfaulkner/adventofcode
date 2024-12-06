e51c015049785c6195de1921a7e1cab8cdc3f0b2
S. Brent Faulkner <brent.faulkner@shopify.com>
2024-12-05 21:08:50 -0500

    ruby solve day 5

--- pkg/day05/day05.rb
+++ pkg/day05/day05.rb
@@ -1,18 +1,67 @@
 class Day05
   def initialize(path)
     fullpath = File.expand_path(path, __dir__)
-    @rows = File.readlines(fullpath).map(&:chomp)
+
+    @rules = Hash.new { |hash, key| hash[key] = Set.new }
+    @updates = []
+
+    lines = File.readlines(fullpath).each
+
+    loop do
+      line = lines.next.chomp
+      break if line.empty?
+
+      before, after = line.split("|").map(&:to_i)
+      @rules[before] << after
+    end
+
+    loop do
+      line = lines.next.chomp
+      break if line.empty?
+
+      @updates << line.split(",").map(&:to_i)
+    end
   end
 
   def part1
-    0
+    sum = 0
+
+    @updates.each do |update|
+      next unless valid_update?(update)
+      sum += update[update.size/2]
+    end
+
+    sum
   end
 
   def part2
-    0
+    sum = 0
+
+    @updates.each do |update|
+      next if valid_update?(update)
+      update.sort! do |a, b|
+        if @rules[a].include?(b)
+          -1
+        elsif @rules[b].include?(a)
+          1
+        else
+          0
+        end
+      end
+      sum += update[update.size/2]
+    end
+
+    sum
   end
 
   private
 
-  # Add private helper methods here
+  def valid_update?(update)
+    preceding = Set.new
+    update.each_with_index do |page, index|
+      return false if @rules[page].intersect?(preceding)
+      preceding << page
+    end
+    true
+  end
 end
--- pkg/day05/day05_test.rb
+++ pkg/day05/day05_test.rb
@@ -9,21 +9,21 @@ require_relative "day05"
 class Day05Test < Minitest::Test
   def test_part1_example1
     input = Day05.new("testdata/example1.txt")
-    assert_equal(0, input.part1)
+    assert_equal(143, input.part1)
   end
 
   def test_part1
     input = Day05.new("testdata/input.txt")
-    assert_equal(0, input.part1)
+    assert_equal(6498, input.part1)
   end
 
   def test_part2_example1
     input = Day05.new("testdata/example1.txt")
-    assert_equal(0, input.part2)
+    assert_equal(123, input.part2)
   end
 
   def test_part2
     input = Day05.new("testdata/input.txt")
-    assert_equal(0, input.part2)
+    assert_equal(5017, input.part2)
   end
 end
